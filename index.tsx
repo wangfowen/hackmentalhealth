32ebe813cb44c4f571fd07ce00a4f48dba4b1f4d
Jamie Wong <jamie.lf.wong@gmail.com>
2018-02-03 14:35:05 -0800

    React setup with HMR

--- index.tsx
+++ index.tsx
@@ -1,1 +1,27 @@
-console.log('Hello!')
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class App extends React.Component<{}, {}> {
+  render() {
+    return <div>
+      Hello World!
+    </div>
+  }
+}
+
+let root: HTMLElement | null = null
+function init() {
+  root = document.createElement('div')
+  document.body.appendChild(root)
+  ReactDOM.render(<App />, root)
+}
+
+if ((module as any).hot) {
+  (module as any).hot.dispose(() => {
+    if (root) {
+      document.body.removeChild(root)
+    }
+  })
+}
+
+init()
