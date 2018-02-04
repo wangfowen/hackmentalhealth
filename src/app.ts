33c4e8645f3b8836b4da6586f4963c8b91675d22
Owen Wang <wangfowen@gmail.com>
2018-02-03 20:52:59 -0800

    graphql api endpoints and static set up

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,41 @@
 import * as express from "express";
+import * as graphqlHTTP from "express-graphql";
 import * as path from "path";
+import {buildSchema} from "graphql";
 
 const app = express();
 
-app.get('/', (req, res) => {
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+};
+
+app.use('/api/users/show', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: false
+}));
+
+app.use('/api/users/index', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.get('/api/users/search', (req, res) => {
+    res.send("TODO");
+});
+
+app.use('/static', express.static('static'));
+
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "../../dev.html"));
 });
 
